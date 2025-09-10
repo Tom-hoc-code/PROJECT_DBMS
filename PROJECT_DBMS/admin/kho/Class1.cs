bca140fa72156cfa2144c4371c0b16473d626481
NguyenHuuTam <huutamand@gmail.com>
2025-09-10 13:17:13 +0700

    no change

---
+++ PROJECT_DBMS/admin/kho/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT_DBMS.admin.kho
+{
+    internal class Class1
+    {
+    }
+}
--- PROJECT_DBMS/obj/Debug/net8.0-windows/PROJECT_DBMS.AssemblyInfo.cs
+++ PROJECT_DBMS/obj/Debug/net8.0-windows/PROJECT_DBMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROJECT_DBMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f51e60708dc6f841c5bc6a20c2b5f3cc24c78e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbbc6dcaad0ceda86e4cd70612e5198c6590bb3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROJECT_DBMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROJECT_DBMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
